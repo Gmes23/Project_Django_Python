3c2b421ac591a56a66989312b0edbce47c25b64e
Gmes23 <gmesda23@gmail.com>
2017-04-10 20:39:39 -0400

    fixed typos in settings.py

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -138,7 +138,7 @@ BOOTSTRAP3 = {
 if os.getcwd() == '/app':
     import dj_database_url
     DATABASES = {
-        'default': dj_database_url.config(default='postgres//localhost')
+        'default': dj_database_url.config(default='postgres://localhost')
     }
 
     # Honor the 'X-Forwarded-Proto' header for request.is_secure().
@@ -151,5 +151,5 @@ if os.getcwd() == '/app':
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     STATIC_ROOT = 'staticfiles'
     STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static')
+        os.path.join(BASE_DIR, 'static'),
     )
